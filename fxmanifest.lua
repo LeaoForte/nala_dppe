d5742bb2495a4f235f05ec933d04e18dbb88d6fb
Carolina Silva <69426278+LeaoForte@users.noreply.github.com>
2025-02-15 14:41:43 +0000

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,19 @@
+fx_version 'cerulean'
+games { 'gta5' }
+
+author 'MoubyCT'
+description 'Riley K9'
+version '1.0.0'
+
+files {
+	'peds.meta'
+}
+
+data_file 'PED_METADATA_FILE' 'peds.meta'
+
+lua54 'yes'
+
+escrow_ignore {}
+dependency '/assetpacks'
+
+dependency '/assetpacks'
